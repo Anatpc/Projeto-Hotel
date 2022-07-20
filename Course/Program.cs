1e4cc0b86ecd9e8ad366719113304a4f5a6f0f6d
Anatpc <ana.tpc2022@gmail.com>
2022-07-20 10:27:10 -0300

    mudança no programa

--- Course/Program.cs
+++ Course/Program.cs
@@ -3,7 +3,7 @@ using Course.Entities;
 
 namespace Course
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
@@ -31,18 +31,13 @@ namespace Course
                 Console.Write("Check-out date (dd/MM/yyyy): ");
                 checkOut = DateTime.Parse(Console.ReadLine());
 
-                DateTime now = DateTime.Now;
-                if (checkIn < now || checkOut < now)
+                string error = reservation.UpdateDates(checkIn, checkOut);
+                if (error != null)
                 {
-                    Console.WriteLine("Error in reservation: Reservation dates for update must be future dates.");
-                }
-                else if (checkOut <= checkIn)
-                {
-                    Console.WriteLine("Error in reservation: Check-out date must be after check-in.");
+                    Console.WriteLine("Error in reservation: " + error);
                 }
                 else
                 {
-                    reservation.UpdateDates(checkIn, checkOut);
                     Console.WriteLine("Reservation: " + reservation);
                 }
             }
